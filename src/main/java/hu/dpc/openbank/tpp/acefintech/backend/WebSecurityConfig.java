82dcbf19fb544da05a0cc0d551a20cc7c793b004
Tamás Sipos <tamas.sipos@dpc.hu>
2019-07-02 16:40:22 +0200

    Basically working with multiple banks

--- src/main/java/hu/dpc/openbank/tpp/acefintech/backend/WebSecurityConfig.java
+++ src/main/java/hu/dpc/openbank/tpp/acefintech/backend/WebSecurityConfig.java
@@ -10,20 +10,13 @@ package hu.dpc.openbank.tpp.acefintech.backend;
 
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 import javax.sql.DataSource;
-import java.util.Arrays;
-import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -36,13 +29,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(final @NotNull HttpSecurity http) throws Exception {
-        http.httpBasic()
-                .and()
-                .authorizeRequests().antMatchers("/netbank/index.html", "/netbank/login", "/netbank/").permitAll()
-                .anyRequest().authenticated()
+        http.httpBasic().and()
+                .authorizeRequests().anyRequest().authenticated()
                 .and().formLogin().loginPage("/netbank/login")
-                .and().logout().permitAll()
-                .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+                .and().logout().permitAll();
+//                .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
     }
 
     @Autowired
@@ -50,36 +41,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             throws Exception {
         auth.jdbcAuthentication()
                 .dataSource(dataSource);
-    }
-
-    /*
-    @org.springframework.context.annotation.Bean
-    public org.springframework.web.servlet.config.annotation.WebMvcConfigurer corsConfigurer() {
-        return new org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(final org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("https://acefintech.mlabs.dpc.hu", "http://localhost")
-                        .allowedMethods("GET", "POST")
-                        .allowedHeaders("x-tpp-bankid")
-                        .exposedHeaders("x-ttp-consentid");
-            }
-        };
-    }
-    */
-
-
-    @Bean
-    public CorsFilter corsFilter() {
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        final CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("*"));
-        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "x-tpp-bankid"));
-        config.setAllowedMethods(Arrays.asList("GET", "POST"));
-        config.setExposedHeaders(Arrays.asList("x-tpp-consentid"));
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
     }
 
 }
